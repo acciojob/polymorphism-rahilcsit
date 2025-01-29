3ce07d258eae6a6e2fe14a65f1c6291ba04f4efe
rahilcsit <rahilcsit@gmail.com>
2025-01-29 22:26:03 +0530

    Spelling mistake

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Prodcut p= new Prodcut();
+        Product p= new Product();
         int ans = p.product(10 , 20);
 
         int ans1 = p.product(10 , 20 , 30);
         double ans2 = p.product(10.5 , 20.5);
         System.out.println(ans + " " + ans1 + " " + ans2);
     }
-    public static class Prodcut{
+    public static class Product {
         public int product(int x,int y){
             return x*y;
         }
