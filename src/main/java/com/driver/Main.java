4103fa54c849b2af20220fcc03af3d069650b2fd
rahilcsit <rahilcsit@gmail.com>
2025-01-29 22:17:48 +0530

    Polymorphism Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Prodcut p= new Prodcut();
+        int ans = p.product(10 , 20);
 
+        int ans1 = p.product(10 , 20 , 30);
+        double ans2 = p.product(10.5 , 20.5);
+        System.out.println(ans + " " + ans1 + " " + ans2 + " ");
+    }
+    public static class Prodcut{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+
+    }
 }
